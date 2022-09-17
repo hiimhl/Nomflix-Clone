ca6def744f22f1f4509ed544fe8d38e177b0beee
hlin <gywjd5126@gmail.com>
2022-09-18 00:35:38 +0900

    Home page - fetch Movie API

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+const API_KEY = "a6266e583d888756aced9d1c2ad96864";
+const BASE_PATH = "https://api.themoviedb.org/3/";
+
+interface IMovies {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovies[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}movie/now_playing?api_key=${API_KEY}`).then((res) =>
+    res.json()
+  );
+}
