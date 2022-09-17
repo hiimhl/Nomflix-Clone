15243f96654df6cdcffb2c4fdb5cfc53ea0e3818
hlin <gywjd5126@gmail.com>
2022-09-17 15:14:38 +0900

    Header - Search bar, Logo, Link button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
-  return <div></div>;
+  return (
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/Routes/Home.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Home() {
+  return <div style={{ backgroundColor: "green", height: "200vh" }}></div>;
+}
+export default Home;
