155f043baf8b1e1962b626f40e3abefc3d601edd
hlin <gywjd5126@gmail.com>
2022-09-18 12:50:51 +0900

    Slider

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
@@ -34,24 +35,104 @@ const Overview = styled.p`
   margin-bottom: 20px;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  position: absolute;
+  width: 100%;
+`;
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  //props를 작성할 때 괄호가 있다면 div 뒤에 타입을 작성.
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center;
+  height: 200px;
+  color: red;
+  font-size: 64px;
+`;
+
+const rowVariants = {
+  //항상 사용자의 모니터화면의 너비를 적용함.//+10은 gap의 값
+  hidden: { x: window.outerWidth + 5 },
+  visible: { x: 0 },
+  exit: { x: -window.outerWidth - 5 },
+};
 //
+
+const offset = 6;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
-  console.log(data, isLoading);
+
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) {
+        //버튼을 클릭하면 다 렌더링 되기전에 슬라이더가 변경되는 에러를 막기위해.
+        return;
+      } else {
+        toggleLeaving(); //true
+        const totalMovies = data?.results.length;
+        const maxIndex = Math.floor(totalMovies / offset) - 1;
+        setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+      }
+    }
+  };
+
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+  //AnimatePresence의 onExitComplete는 exit가 완료된 후 실행됨.
+
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
-        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-          <Title>{data?.results[0].title}</Title>
-          <Overview>{data?.results[0].overview}</Overview>
-        </Banner>
+        <>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                    />
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
       )}
     </Wrapper>
   );
 }
 export default Home;
+//4:45
